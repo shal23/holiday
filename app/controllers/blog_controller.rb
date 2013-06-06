bb1d3720a28e4c04f9be12c21221e496ffa23224
shal23 <dr.shalinidalal@gmail.com>
2013-06-06 16:54:11 +1000

    gem

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,5 +3,6 @@ class BlogController < ApplicationController
   def index
   	
   	@posts = Post.all
+  	@posts = Post.where(:active => true).paginate(:page => params[:page]).order('created_at DESC')
   end
 end
